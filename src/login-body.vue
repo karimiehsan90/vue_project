8bce74d7f81553e8a85097ac2feb1e8c218bb55f
unknown <alisdg1376@gmail.com>
2019-06-10 12:43:33 +0430

    forget password front and linked

--- src/login-body.vue
+++ src/login-body.vue
@@ -38,10 +38,38 @@
                     <span class="text-blue">عضویت</span><img src="./img/user-blue.png" class="person-icon" onclick="">
                     </router-link>
                 </form>
+                <button class="btn btn-link float-left" data-toggle="modal" data-target="#forgetModal">! رمز عبورم را فراموش کردم </button>
             </div>
         </div>
         <div class="col-lg-3"></div>
     </div>
+        <div id="forgetModal" class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h6 class="modal-title mx-auto">بازیابی رمز عبور</h6>
+                        <button type="button" class="close float-left small position-absolute" style="left: 5px ; top: 10px;" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group" v-if="showPhone">
+                            <label class="float-right small">: موبایل خود را وارد کنید</label>
+                            <input class="form-control-sm form-control" type="text" v-model="fphone">
+                            <br>
+                            <button class="btn btn-outline-info btn-sm btn-block" @click="sendSMS()">ادامه</button>
+                        </div>
+                        <div class="form-group" v-if="showCode">
+                            <label class="float-right small"> : کد فرستاده شده را وارد کنید </label>
+                            <input class="form-control-sm form-control" type="text" v-model="fcode">
+                            <br>
+                            <button class="btn btn-outline-info btn-sm btn-block" @click="sendCODE()">ادامه</button>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <p class="small mx-auto" v-bind:class="[msgCol ? 'text-success':'text-danger']">{{msg}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -52,7 +80,13 @@
         data() {
             return {
                 email: '',
-                password: ''
+                password: '',
+                fphone:'',
+                fcode:'',
+                showPhone:true,
+                showCode:false,
+                msg:'',
+                msgCol:false
             }
         },
         methods: {
@@ -96,12 +130,48 @@
                     var auth2 = gapi.auth2.getAuthInstance();
                     auth2.signOut();
                 });
-            }
+            },
+            sendSMS(){
+                var vm = this;
+                $.post('/ticketing/rest/auth/forget', {
+                    phone: vm.fphone
+                }).done(function (data) {
+                    if (data.success){
+                        vm.showPhone=false;
+                        vm.showCode=true;
+                    }
+                    vm.msgCol=data.success;
+                    vm.msg=data.message;
+                });
+            },
+            sendCODE(){
+                var vm = this;
+                $.post('/ticketing/rest/auth/forget/submit', {
+                    phone: vm.fphone,
+                    code: vm.fcode
+                }).done(function (data) {
+                    if (data.success){
+                        vm.showPhone=true;
+                        vm.showCode=false;
+                        vm.fcode='';
+                        vm.fphone='';
+                    }
+                    vm.msgCol=data.success;
+                    vm.msg=data.message;
+                });
+            },
         },
         mounted() {
             gapi.signin2.render('google-login', {
                 onsuccess: this.onSignIn
-            })
+            });
+            $('#forgetModal').on('show.bs.modal', function (event) {
+                if (this.showPhone){
+                    this.msg='';
+                    this.fphone='';
+                }
+
+            });
         }
     }
 </script>
